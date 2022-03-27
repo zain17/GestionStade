eb91f3fa4f23291d33973ab1de90d2043f8bcaf9
zine <zainelabidine.bensaleh@esprit.tn>
2022-03-27 19:40:25 +0200

    init

---
+++ src/main/java/tn/stade/GestionstadeApplication.java
@@ -0,0 +1,13 @@
+package tn.stade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionstadeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionstadeApplication.class, args);
+    }
+
+}
